e2e180e950ba5407c94cbec87a52054cf35c6fb8
TeonaTanasa <teona.tanasa@principal33.com>
2021-09-16 20:39:45 +0300

    created initial project.

---
+++ CMS-back/CMS-back/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMS_back.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
